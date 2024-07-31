adac5ff9ef4312edde59bcdb79440780c6343b29
YOMorales <yomorales@gmail.com>
2024-07-31 14:04:49 -0500

    Adding Stripe Clock ID var to config, as it is needed in API calls.

--- config/stripe.php
+++ config/stripe.php
@@ -6,4 +6,5 @@ Also, I created a custom config file for new env variables as that is a usual La
 return [
     'public_key' => env('STRIPE_PUBLIC_KEY'),
     'secret_key' => env('STRIPE_SECRET_KEY'),
+    'test_clock' => env('STRIPE_TEST_CLOCK'),
 ];
