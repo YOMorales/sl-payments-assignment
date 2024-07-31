01977ed135c56ebc8923dcedd5201e4fee3ea009
YOMorales <yomorales@gmail.com>
2024-07-31 12:40:49 -0500

    Added new configs for Stripe keys.

---
+++ config/stripe.php
@@ -0,0 +1,9 @@
+<?php
+/*
+YOM: I could have stored this on services.php, but I wanted to show how to create a new config file.
+Also, I created a custom config file for new env variables as that is a usual Laravel practice.
+*/
+return [
+    'public_key' => env('STRIPE_PUBLIC_KEY'),
+    'secret_key' => env('STRIPE_SECRET_KEY'),
+];
